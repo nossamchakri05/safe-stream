b56b2d0c129be38372bb9d6ecd8f7e3a97948039
GitHub User <user@github.com>
2026-01-03 11:34:06 +0530

    Fix: Use VITE_API_URL environment variable in frontend API config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://${window.location.hostname}:5000/api`,
+    baseURL: import.meta.env.VITE_API_URL || `http://${window.location.hostname}:5000/api`,
 });
 
 api.interceptors.request.use((config) => {
